fe8cfacbc37660d6f47ee6b3d1be7193b65c32f8
Samyak Meshram <samyakmeshram008@Samyaks-MacBook-Pro-2.local>
2025-06-11 18:24:27 +0530

    ft-added the agents table

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -6,21 +6,23 @@ import Loading from "@/components/loading-state";
 import ErrorState from "@/components/error-state";
 import ResponsiveDialog from "@/components/responsive-dialog";
 import { Button } from "@/components/ui/button";
-
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import EmptyState from "@/components/emptyState";
 const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.agents.queryOptions());
+  console.log(data, "the agent data");
   return (
-    <div>
-      <ResponsiveDialog
-        open={false}
-        title="Responsive Test"
-        description="Responsive Descriptiom"
-        onOpenChange={() => {}}
-      >
-        <Button>Some Action</Button>
-      </ResponsiveDialog>
-      {JSON.stringify(data)}
+    <div className="flex pb-4 px-4 md:px-8 flex-1 flex-col gap-y-4">
+      {data.length === 0 ? (
+        <EmptyState
+          title="Create your first Agent"
+          description="Create an agent to join in your meetings. Each agent will follow your instructions"
+        />
+      ) : (
+        <DataTable columns={columns} data={data} />
+      )}
     </div>
   );
 };
